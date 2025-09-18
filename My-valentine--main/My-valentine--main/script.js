92afea2f3e84a753152aee5f8a99c8aad16736e1
JasonLiew-king <64052387+JasonLiew-king@users.noreply.github.com>
2025-09-18 16:41:32 +0800

    Update script.js

--- My-valentine--main/My-valentine--main/script.js
+++ My-valentine--main/My-valentine--main/script.js
@@ -26,6 +26,6 @@ function handleNoClick() {
 }
 
 function handleYesClick() {
-    window.location.href = "https://jasonliew.github.io/project_anniversary_2025/My-valentine--main/My-valentine--main/yes_page.html";
+    window.location.href = "https://jasonliew-king.github.io/project_anniversary_2025/My-valentine--main/My-valentine--main/yes_page.html";
 }
 
